ca270dd318c1b95bc5d81bd17f86150f5fc362f2
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-24 20:08:31 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -5,6 +5,39 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+let likes;
+const errModal = document.getElementById("modal");
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+  likes = document.getElementsByClassName("like")
+  // adds Event Listener to each like <li>
+  for (let i=0; i<likes.length; i++) {
+    likes[i].addEventListener("click", () =>{
+      let heart = likes[i].querySelector("span");
+      //event listener does the following conditions below
+
+      if (heart.innerHTML === EMPTY_HEART) {
+        mimicServerCall()
+          .then(response => {
+            console.log(response)
+            heart.innerHTML = FULL_HEART;
+            heart.className = "activated-heart"
+          })
+          .catch(error =>{
+            let errMessage = document.getElementById("modal-message")
+            errMessage.innerText = error
+            errModal.className = ""
+            setTimeout( () =>{errModal.className = "hidden"}, 5000)
+          })
+
+      } else {
+        heart.innerHTML = EMPTY_HEART;
+        heart.className = "like-glyph"
+      }
+    })
+  };
+});
 
 
 //------------------------------------------------------------------------------
